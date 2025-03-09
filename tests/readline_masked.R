e74f96c929030f6db28469c3a8562b1e00613641
lilyclements <lily@idems.international>
2025-03-09 20:03:45 +0000

    Create readline_masked.R

---
+++ tests/readline_masked.R
@@ -0,0 +1,22 @@
+# From original getPass directory test file: https://github.com/wrathematics/getPass/blob/master/tests/readline_masked.r
+### We can't run the test on Windows because we have to pipe the output into a new process and I don't think that's possible :[
+if (tolower(.Platform$OS.type) == "windows"){
+  invisible(TRUE)
+} else {
+  passpassword <- function(pw, msg="", showstars=FALSE, noblank=FALSE)
+  {
+    syscmd <- paste0("echo '", pw, "' | ${R_HOME}/bin${R_ARCH_BIN}/Rscript -e 'cat(getPass:::readline_masked_term(msg=", paste0("\"", msg, "\""), ",", showstars, ",", noblank, "))'")
+    system(syscmd, intern=TRUE)
+  }
+  
+  pw <- 'asdf'
+  test <- passpassword(pw)
+  stopifnot(identical(test, pw))
+  
+  pw.preblank <- paste0("\n\n", pw)
+  test <- passpassword(pw.preblank)
+  stopifnot(identical(test, character(0)))
+  
+  test <- passpassword(pw.preblank, noblank=TRUE)
+  stopifnot(identical(test, pw))
+}
